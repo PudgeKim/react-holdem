20197e2634b04faa92dd6017f2e0ad0e11aed6f8
PudgeKim <zxaq15@gmail.com>
2021-09-22 23:41:31 +0900

    erase annotation

--- src/components/game-room/GameRoomPage.js
+++ src/components/game-room/GameRoomPage.js
@@ -1,24 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useHistory } from "react-router-dom";
-//import io from "socket.io-client";
-//import { config } from "../../config";
 import "./GameRoomPage.css";
 import PlayerBoxes from "./PlayerBoxes";
 import { addGetUsersInfoEvent, initSocket, leaveRoom } from "./socket";
 
-//const socket = io(config.baseURL + "/holdem-room");
-
 function GameRoomPage() {
   const history = useHistory();
-  //const socket = useRef(null);
   const location = useLocation();
   const roomId = location.state.roomId;
   const roomName = location.state.roomName;
   const user = location.state.user;
   const [isHost, setHost] = useState(false);
   const [isGameStart, setGameStart] = useState(false);
-  //let leave;
-  //socket = io(config.baseURL + "/holdem-room");
 
   const [players, setPlayers] = useState([]);
 
@@ -51,27 +44,7 @@ function GameRoomPage() {
     return playerArr;
   };
 
-  // const leaveRoom = () => {
-  //   socket.emit("leaveRoom", {
-  //     roomId: roomId,
-  //     nickname: user.nickname,
-  //   });
-
-  //   history.goBack();
-  // };
-
   useEffect(() => {
-    // socket.emit("joinRoom", {
-    //   roomId: roomId,
-    //   roomName: roomName,
-    //   userId: user.userId,
-    //   nickname: user.nickname,
-    // });
-    // socket.on("getUsersInfo", (usersInfo) => {
-    //   console.log("getUsersInfo event !!");
-    //   const playerArr = getGamePlayers(usersInfo);
-    //   setPlayers(playerArr);
-    // });
     initSocket(roomId, roomName, user);
     addGetUsersInfoEvent(getGamePlayers, setPlayers);
   }, []);
